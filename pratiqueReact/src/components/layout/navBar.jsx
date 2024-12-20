f399f3d6af5baaf6a5807831002e9155ceeae33a
LaigleJ <you@example.com>
2024-12-20 12:31:26 +0100

    mise à jour

--- pratiqueReact/src/components/layout/navBar.jsx
+++ pratiqueReact/src/components/layout/navBar.jsx
@@ -3,8 +3,25 @@ import { NavLink } from "react-router";
 export function NavBar() {
   return (
     <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <nav className="bg-gray-800 p-4">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
+          {/* Menu de navigation */}
+          <div className="space-x-6 hidden md:flex">
+            <NavLink
+              className="text-white hover:text-red-500 transition-colors duration-300"
+              to="/"
+            >
+              Accueil
+            </NavLink>
+            <NavLink
+              className="text-white hover:text-red-500 transition-colors duration-300"
+              to="/about"
+            >
+              À propos
+            </NavLink>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
